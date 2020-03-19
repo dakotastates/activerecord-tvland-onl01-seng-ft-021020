523e3453c22b2459a477b13f6fce39df7ab98ca1
Dakota States <dakota.states@gmail.com>
2020-03-19 08:36:20 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,14 @@
 class Show < ActiveRecord::Base
+
+  belongs_to :network
+  has_many :characters
+  has_many :actors, through: :characters
   
+  
+    def actors_list
+      actors = []
+      self.actors.collect {|t| actors << t.full_name}
+      actors
+    end
+
 end
